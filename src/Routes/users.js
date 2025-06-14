6ff6ee3144b7c295dbf719da66bd5beacb81a5e1
Obafemi <dadaobafemi26@gmail.com>
2025-06-14 22:07:58 +0100

    installed packages

--- src/Routes/users.js
+++ src/Routes/users.js
@@ -21,3 +21,21 @@ app.post('/users', async (req, res) => {
     res.status(500).json({ error: 'Database error' });
   }
 });
+
+app.get('/users/:id', async (req, res) => {
+  const {id} = req.params
+
+  try {
+    const [rows] = await pool.query(
+      'SELECT * FROM users WHERE id = ?', [id]
+    )
+    if (rows.length === 0) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+    res.json(rows[0])
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Database error' });
+  }
+
+})
