ae8ee1b2fab6369278a855d3b3fff89dacdb82c7
Obafemi <dadaobafemi26@gmail.com>
2025-06-15 15:05:59 +0100

    updated server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import authRoutes from './src/Routes/authRoutes.js'
 import messageRoutes from './src/Routes/messageRoutes.js'
 
 export const app = express();
-const PORT = 3000;
+const PORT = 3000 || 5000;
 
 //middleware
 app.use(express.json());
@@ -17,15 +17,17 @@ app.get("/", (req, res) => {
   res.send("Hello");
 });
 
+app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+
 mongoose
   .connect(
     "mongodb+srv://obafemi:obafemi@backend.k4juqxb.mongodb.net/?retryWrites=true&w=majority&appName=Backend"
   )
   .then(() => {
     console.log("connected to database");
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
-    });
+    
   })
   .catch((error) => {
     console.error(error + "Could not connect");
