25dcd79238a86143006e78c7ad7808e8bbcc492d
Obafemi <dadaobafemi26@gmail.com>
2025-06-17 10:41:26 +0100

    added error handling for existing email

--- src/Routes/authRoutes.js
+++ src/Routes/authRoutes.js
@@ -7,11 +7,15 @@ const router = express.Router();
 
 router.post('/register', async (req, res) => {
     try {
-        const {username, password} = req.body;
+        const {username, password, email} = req.body;
         const existingUser = await userModel.findOne({username})
+        const existingEmail = await userModel.findOne({email})
 
         if(existingUser){
            return res.status(400).json({message: 'User already exists'})
+        }
+        if(existingEmail){
+          return res.status(400).json({message: "email already in use"})
         }
 
         const hashedPassword = await bcrypt.hash(password, 10);
@@ -31,12 +35,12 @@ router.post('/login', async (req, res) => {
       return res.status(400).json({ message: 'Invalid credentials' });
     }
 
-    const passwordMatch = await bcrypt.compare(password, user.password); // ✅ await it
+    const passwordMatch = await bcrypt.compare(password, user.password); 
     if (!passwordMatch) {
       return res.status(400).json({ message: 'Invalid credentials' });
     }
 
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET); // ✅ use user._id
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET); 
 
     res.json({
       token,
