04d6496633c4428c4ab6ee40af06e597e8b78971
Obafemi <dadaobafemi26@gmail.com>
2025-06-21 13:37:55 +0100

    added notes route

--- src/Routes/notesRoutes.js
+++ src/Routes/notesRoutes.js
@@ -3,4 +3,27 @@ import notesModel from '../models/notes.model.js'
 
 const router = express.Router()
 
+router.post('/create', async (req, res) => {
+    try {
+        const { title, content, category, user} = req.body;
+        const newNote = new notesModel({
+            title,
+            content,
+            category,
+            user
+        });
+        await newNote.save();
+        res.status(201).json({ message: 'Note created successfully', note: newNote });
+    } catch (error) {
+        res.status(500).json({ error: "Error creatting note"});
+    }
+})
+router.get('/', async (req, res) => {
+    try {
+        const notes = await notesModel.find().populate('user', 'username email');
+        res.status(200).json(notes);
+    } catch (error) {
+        res.status(500).json({ error: "Error fetching notes" });
+    }
+})
 export default router
